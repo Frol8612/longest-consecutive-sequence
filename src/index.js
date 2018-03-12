8d61af531cfc2220787bbd9ff12470b3794b2e60
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-03-12 10:16:06 +0300

    longest

--- src/index.js
+++ src/index.js
@@ -1,41 +1,52 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
-  if (array.length == 0) {
-    return 0;
-  }
+  if (array.length == 0) return 0;
 
-  if (array.length == 1) {
-    return 1;
-  }
-  let s = array.sort((a, b) => {return a - b});
-      stack = [],
+  if (array.length == 1) return 1;
+
+  array = array.sort((a, b) => {return a - b});
+  let stack = [],
       l ={},
-      n = [];
+      n = [],
+      s = doubleDel(array);
 
-
-  for (let i = 0; i < s.length; i++) {
-
-    if (s[i] + 1 == s[i+1] || s[i] - 1 == s[i-1]) {
-        stack.push(s[i]);
-        
-      if (s[i] + 1 != s[i+1]) {
-        if (l[stack.length] == undefined) {
-          l[stack.length] = 1;
-        } else {
-          l[stack.length]++;
-        }
-      stack = [];
+  function doubleDel(arr) {
+      let obj = {},
+          one = [],
+          j = 0;
+      for (let i = 0; i < arr.length; i++) {
+           let item = arr[i];
+           if( obj[item] !== 1) {
+                 obj[item] = 1;
+                 one[j++] = item;
+           }
       }
-    }   
+      return one;
+  }
+  for (let i = 0; i < s.length; i++) {
+      
+      if (s[i] + 1 == s[i+1] || s[i] - 1 == s[i-1]) {
+              stack.push(s[i]);
+              if (s[i] + 1 != s[i+1]) {
+                  if (l[stack.length] == undefined) {
+                      l[stack.length] = 1;
+                  } else {
+                      l[stack.length]++;
+                  }      
+                  stack = [];
+              }
+         
+      }   
+      
   } 
 
   let mid = [];
   for (const key in l) {
-    mid.push(Number(key));
+          mid.push(Number(key));
   }
   if (mid.length == 0) {
     return 1;
   }
-
+  
   return Math.max.apply(null, mid);
 }
